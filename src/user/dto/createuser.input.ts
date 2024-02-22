eeb8411c28408cfec6f1d3b26d0797f1a64aaa5f
zacro14 <sacrojunel@protonmail.com>
2024-02-22 14:48:49 +0800

    feat: create user

--- src/user/dto/createuser.input.ts
+++ src/user/dto/createuser.input.ts
@@ -1,6 +1,13 @@
-import { Field } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
+@InputType()
 export class CreateUserInput {
+  @IsNotEmpty()
+  @IsEmail()
   @Field()
   email: string;
+
+  @Field({ nullable: true })
+  name: string;
 }
--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,13 +1,14 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { User } from './models/user.model';
 import { CreateUserInput } from './dto/createuser.input';
+import { User } from './models/user.model';
 import { UserService } from './user.service';
 
 @Resolver(() => User)
 export class UserResolver {
   constructor(private userService: UserService) {}
+
   @Mutation(() => User)
-  async createUser(@Args() args: CreateUserInput): Promise<User | null> {
+  async createUser(@Args('user') args: CreateUserInput): Promise<User | null> {
     return this.userService.createUser(args);
   }
 }
